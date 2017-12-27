63f33de3f26a13944050f1fa235d2a5b74c6810c
Finch <finch@play-net.org>
2017-12-27 04:40:56 +0100

    Add primitive for writing files

--- pkg/primitives/fs.go
+++ pkg/primitives/fs.go
@@ -1,13 +1,14 @@
 package primitives
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"go.uber.org/zap"
 )
 
-// OpenFile returns a primitive function for opening files
+// OpenFile returns a primitive function for opening and reading files
 func OpenFile(log *zap.Logger) func(path string) (*os.File, error) {
 	return func(path string) (*os.File, error) {
 		path, err := filepath.Abs(path)
@@ -15,6 +16,18 @@ func OpenFile(log *zap.Logger) func(path string) (*os.File, error) {
 			log.Error("could not get abs path", zap.String("file", path), zap.Error(err))
 			return nil, err
 		}
-		return os.OpenFile(path, os.O_RDWR, 0666)
+		return os.OpenFile(path, os.O_RDONLY, 0666)
+	}
+}
+
+// WriteFile returns a primitive function for writing files replacing their content
+func WriteFile(log *zap.Logger) func(path string, data []byte) error {
+	return func(path string, data []byte) error {
+		path, err := filepath.Abs(path)
+		if err != nil {
+			log.Error("could not get abs path", zap.String("file", path), zap.Error(err))
+			return err
+		}
+		return ioutil.WriteFile(path, data, 0666)
 	}
 }
